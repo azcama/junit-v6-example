7cbe66c9c47ede3c7f9913c255bb98c886c5f37e
azcama <azaharacm7@gmail.com>
2026-02-24 13:11:06 +0100

    Session 25-2-26: Keeping basic tests only

--- src/test/java/com/example/project/CalculatorTests.java
+++ src/test/java/com/example/project/CalculatorTests.java
@@ -11,11 +11,8 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
 
@@ -26,16 +23,4 @@ class CalculatorTests {
 		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
 	}
 
-	@ParameterizedTest(name = "{0} + {1} = {2}", quoteTextArguments = false)
-	@CsvSource(textBlock = """
-			0,    1,   1
-			1,    2,   3
-			49,  51, 100
-			1,  100, 101
-			""")
-	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
-		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
-	}
 }
